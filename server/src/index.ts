439b2d1fa3bc6936657e49d9913086bdbfaf2015
Maks Dul <maxoxxam@gmail.com>
2025-06-08 23:12:07 +0200

    deleted unused import

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,6 @@ import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import WebSocket, { WebSocketServer } from 'ws';
 import { v4 as uuidv4 } from 'uuid';
-import { error } from 'console';
 
 dotenv.config();
 
